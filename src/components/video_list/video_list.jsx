992103ebee140409d236124ee679452162c1252d
HyukHyunCho <gurgus0189@gmail.com>
2022-08-10 00:27:32 +0900

    video add

---
+++ src/components/video_list/video_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoItem from '../video_item/video_item';
+
+const VideoList = props => (
+  <ul>
+    console.log(props)
+    {props.videos.map(video => (
+      <VideoItem key={video.id} video={video} />
+    ))}
+  </ul>
+);
+
+export default VideoList;
