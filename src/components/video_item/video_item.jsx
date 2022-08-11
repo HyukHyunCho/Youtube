527092945e2b53692b00fa95e21aad3440e6367b
HyukHyunCho <gurgus0189@gmail.com>
2022-08-12 00:39:07 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styles from './video_item.module.css'; // 같은 폴더안에있는 css
 
-const VideoItem = props => (
-    <h1>{props.video.snippet.title}</h1>
+// props: props.video.snippet.thumbnails.medium.url
+// -> video: { snippet } => snippet.thumbnails.medium.url
+// -> 디컨스트럭팅
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
 );
 
 export default VideoItem;
